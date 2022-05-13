a417a09e3512d176b54d9ce4f93ce373fff8b1cd
Lucas Billard <46447981+Azukiro@users.noreply.github.com>
2022-05-13 13:36:39 +0200

    Authentification

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,13 +1,23 @@
-from flask import Flask
-
+from flask import Flask, request
+from dbServices import DBServices
+from jwt_utils import build_token, decode_token
+from requestServices import post_question, get_questions, get_question
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    x = 'world'
-    return f"Hello, {x}"
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+    return {"size": 0, "scores": []}, 200
+
+
+@app.route('/login', methods=['POST'])
+def Login():
+    payload = request.get_json()
+    password = payload['password']
+    if password == "Vive l'ESIEE !":
+        return {"token": build_token()}, 200
+    return build_token(), 401
 
 
 if __name__ == "__main__":
-    app.run(ssl_context='adhoc')
+    app.run(ssl_context='adhoc', use_reloader=True, debug=True)
